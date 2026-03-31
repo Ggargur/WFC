bea6bf94e5b517ba5189cabed0aded2ba13e724a
Gabriel Gargur <tavares.gargur@gmail.com>
2026-03-31 00:10:48 -0300

    Fixed state by state calculations

--- MapCreator.cs
+++ MapCreator.cs
@@ -24,7 +24,6 @@ namespace WaveFunction
 
         private readonly Stopwatch _currentStopWatch = new();
         private readonly Stack<Cell> _instancedCells = new();
-        private Stack<List<Cell>> _chunkStack = new();
         private bool IsDone => InstancedCells.Count == _grid.Length;
         [SerializeField] private int maxIterations = 1000;
         private Cell[] _grid;
@@ -34,15 +33,12 @@ namespace WaveFunction
 
         public const int MaxTurnAmount = 4;
         private System.Random _systemRandom = new();
-        private bool HasCreatedMistake => !IsDone && _grid.Any(x => !x.IsCollapsable && !x.IsCollapsed);
         public List<Cell> InstancedCells => _instancedCells.ToList();
         private int GridMagnitude => matrixSize.x * matrixSize.y * matrixSize.z;
-
 
         private void Start()
         {
             if (useRandomizedSeed) return;
-
             _systemRandom = new System.Random(seed);
         }
 
@@ -50,7 +46,6 @@ namespace WaveFunction
         {
             Dictionary<Direction, Cell> neighbors = new();
             var index = Array.IndexOf(_grid, cell);
-
             if (index < 0) return neighbors;
 
             var (x, y, z) = GetPosition(index);
@@ -59,8 +54,8 @@ namespace WaveFunction
             {
                 var offset = dir.ToVector3();
                 var neighborPos = pos + offset;
-
-                if (IsInBounds(neighborPos)) neighbors.TryAdd(dir, _grid[GetIndex(neighborPos)]);
+                if (IsInBounds(neighborPos))
+                    neighbors.TryAdd(dir, _grid[GetIndex(neighborPos)]);
             }
 
             return neighbors;
@@ -79,17 +74,10 @@ namespace WaveFunction
             return options.Any() ? options.GetRandom(_systemRandom) : null;
         }
 
-        private void PopStack()
-        {
-            var forcedOption = _instancedCells.Pop();
-            forcedOption.Uncollapse();
-        }
-
         private static IEnumerable<Cell> GetNextOptions(IEnumerable<Cell> chunk)
         {
             var allOptions = chunk.Where(x => x.IsCollapsable).ToList();
-            if (!allOptions.Any())
-                return allOptions;
+            if (!allOptions.Any()) return allOptions;
 
             var min = allOptions.Min(x => x.Entropy);
             return allOptions.Where(x => Mathf.Approximately(x.Entropy, min));
@@ -103,17 +91,16 @@ namespace WaveFunction
 
             CreateRoutine().ContinueWith(t =>
             {
-                if (t.IsFaulted)
-                {
-                    Debug.LogException(t.Exception);
-                }
+                if (t.IsFaulted) Debug.LogException(t.Exception);
             }, TaskScheduler.FromCurrentSynchronizationContext());
         }
 
         private async Task CreateRoutine()
         {
             _currentStopWatch.Start();
-            _grid = GetGrid().ToArray();
+            
+            _grid = new Cell[GridMagnitude];
+            InitGrid();
 
             await CollapseAll();
 
@@ -121,29 +108,31 @@ namespace WaveFunction
             _currentStopWatch.Stop();
             _currentStopWatch.Reset();
         }
+        
+        private void InitGrid()
+        {
+            for (var i = 0; i < matrixSize.x; i++)
+            for (var j = 0; j < matrixSize.y; j++)
+            for (var k = 0; k < matrixSize.z; k++)
+            {
+                _grid[GetIndex(i, j, k)] = new Cell(new Vector3Int(i, j, k), _systemRandom);
+            }
+        }
 
         private async Task CollapseAll()
         {
             var chunks = GetChunks().ToList();
-
             foreach (var chunk in chunks)
-            {
                 await SolveChunk(chunk);
-            }
 
             if (TryGetComponent(out Instantiator instantiator))
-            {
                 instantiator.InstantiateCells(_grid.Where(c => c.IsCollapsed).ToList());
-            }
         }
-
 
         private void ResetChunk(List<Cell> chunk)
         {
             foreach (var cell in chunk)
-            {
                 cell.Uncollapse();
-            }
         }
 
         private IEnumerable<List<Cell>> GetChunks()
@@ -161,7 +150,6 @@ namespace WaveFunction
                 for (int k = 0; k < chunkSize.z; k++)
                 {
                     var pos = new Vector3Int(x + i, y + j, z + k);
-
                     if (IsInBounds(pos))
                         chunk.Add(_grid[GetIndex(pos)]);
                 }
@@ -171,58 +159,72 @@ namespace WaveFunction
 
             return chunks;
         }
-
+        
         private async Task SolveChunk(List<Cell> chunk)
         {
-            int iteration = 0;
-
-            while (chunk.Any(c => !c.IsCollapsed))
+            for (int attempt = 0; attempt < maxChunkRetries; attempt++)
             {
-                if (iteration++ > maxIterations)
-                    throw new UnfortunateSeedException();
+                var chunkStack = new Stack<Cell>();
+                bool failed = false;
+                int iteration = 0;
 
-                var next = SelectNext(chunk);
+                while (chunk.Any(c => !c.IsCollapsed))
+                {
+                    if (iteration++ > maxIterations)
+                    {
+                        failed = true;
+                        break;
+                    }
 
-                Collapse(next);
+                    var next = SelectNext(chunk);
 
-                await Task.Yield();
+                    if (!CollapseInChunk(next, chunkStack))
+                    {
+                        failed = true;
+                        break;
+                    }
+
+                    await Task.Yield();
+                }
+
+                if (!failed)
+                    return;
+                
+                ResetChunk(chunk);
             }
+
+            throw new UnfortunateSeedException();
         }
-
-
-        private void Collapse(Cell next)
+        
+        private bool CollapseInChunk(Cell next, Stack<Cell> chunkStack)
         {
             while (true)
             {
                 if (next == null)
-                    throw new UnfortunateSeedException();
+                    return false;
 
                 if (next.Collapse())
                 {
+                    chunkStack.Push(next);
                     _instancedCells.Push(next);
-
-                    if (!HasCreatedMistake)
-                        return;
+                    
+                    if (!NeighborsHaveMistake(next))
+                        return true;
                 }
-                
-                if (_instancedCells.Count == 0)
-                    throw new UnfortunateSeedException();
 
-                next = _instancedCells.Pop();
+                if (chunkStack.Count == 0)
+                    return false;
+
+                // Backtrack somente dentro do chunk
+                next = chunkStack.Pop();
+                _instancedCells.TryPop(out _);
                 next.Uncollapse();
             }
         }
-
-
-        private IEnumerable<Cell> GetGrid()
+        
+        private bool NeighborsHaveMistake(Cell cell)
         {
-            _grid = new Cell[GridMagnitude];
-            for (var i = 0; i < matrixSize.x; i++)
-            for (var j = 0; j < matrixSize.y; j++)
-            for (var k = 0; k < matrixSize.z; k++)
-            {
-                yield return new Cell(new Vector3Int(i, j, k), _systemRandom);
-            }
+            return cell.Neighbors.Values.Any(n => !n.IsCollapsable && !n.IsCollapsed);
         }
 
         [ContextMenu("Clear")]
